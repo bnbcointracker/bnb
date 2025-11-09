d8838f39ab32f8d6b0966ad30f742bb20405c97c
bnbcointracker <Bnbcointracker@gmail.com>
2025-11-09 01:41:16 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,149 @@
+// --------------------  -------------------- 
+    // 
+    $(window).scroll(function() {     
+        var scroll = $(window).scrollTop();
+        if (scroll > 0) {
+            $(".header-nav").addClass("active");
+        }
+        else {
+            $(".header-nav").removeClass("active");
+        }
+    });
+    
+    // 
+    function homeOpen() {
+        document.getElementById("home-menu").style.display = "flex";
+    }
+    function homeClose() {
+        document.getElementById("home-menu").style.display = "none";
+    }    
+    // -------------------- 
+//  -------------------- -------------------- --------------------   
+
+
+
+
+
+// --------------------  -------------------- 
+    // 
+    function About() {
+        document.getElementById("about-us").style.display = "flex";
+        document.getElementById("terms").style.display = "none";
+        document.getElementById("privacy-policy").style.display = "none";
+        document.getElementById("conditions").style.display = "none";
+        document.getElementById("commitment").style.display = "none";
+        document.getElementById("legal").style.display = "none";
+
+        document.getElementById("underline1").style.borderBottom = "2px solid var(--yellow-background)";
+        document.getElementById("underline2").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline3").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline4").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline5").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline6").style.borderBottom = "2px solid transparent";
+    }
+    // --------------------
+     
+    // 
+    function Terms() {
+        document.getElementById("about-us").style.display = "none";
+        document.getElementById("terms").style.display = "flex";
+        document.getElementById("privacy-policy").style.display = "none";
+        document.getElementById("conditions").style.display = "none";
+        document.getElementById("commitment").style.display = "none";
+        document.getElementById("legal").style.display = "none";
+
+        document.getElementById("underline1").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline2").style.borderBottom = "2px solid var(--yellow-background)";
+        document.getElementById("underline3").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline4").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline5").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline6").style.borderBottom = "2px solid transparent";
+    }
+    // --------------------
+     
+    // 
+    function Policy() {
+        document.getElementById("about-us").style.display = "none";
+        document.getElementById("terms").style.display = "none";
+        document.getElementById("privacy-policy").style.display = "flex";
+        document.getElementById("conditions").style.display = "none";
+        document.getElementById("commitment").style.display = "none";
+        document.getElementById("legal").style.display = "none";
+
+        document.getElementById("underline1").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline2").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline3").style.borderBottom = "2px solid var(--yellow-background)";
+        document.getElementById("underline4").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline5").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline6").style.borderBottom = "2px solid transparent";
+    }
+    // --------------------
+     
+    // 
+    function Conditions() {
+        document.getElementById("about-us").style.display = "none";
+        document.getElementById("terms").style.display = "none";
+        document.getElementById("privacy-policy").style.display = "none";
+        document.getElementById("conditions").style.display = "flex";
+        document.getElementById("commitment").style.display = "none";
+        document.getElementById("legal").style.display = "none";
+
+        document.getElementById("underline1").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline2").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline3").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline4").style.borderBottom = "2px solid var(--yellow-background)";
+        document.getElementById("underline5").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline6").style.borderBottom = "2px solid transparent";
+    }
+    // --------------------
+     
+    // 
+    function Commitment() {
+        document.getElementById("about-us").style.display = "none";
+        document.getElementById("terms").style.display = "none";
+        document.getElementById("privacy-policy").style.display = "none";
+        document.getElementById("conditions").style.display = "none";
+        document.getElementById("commitment").style.display = "flex";
+        document.getElementById("legal").style.display = "none";
+
+        document.getElementById("underline1").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline2").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline3").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline4").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline5").style.borderBottom = "2px solid var(--yellow-background)";
+        document.getElementById("underline6").style.borderBottom = "2px solid transparent";
+    }
+    // --------------------
+     
+    // 
+    function Legal() {
+        document.getElementById("about-us").style.display = "none";
+        document.getElementById("terms").style.display = "none";
+        document.getElementById("privacy-policy").style.display = "none";
+        document.getElementById("conditions").style.display = "none";
+        document.getElementById("commitment").style.display = "none";
+        document.getElementById("legal").style.display = "flex";
+
+        document.getElementById("underline1").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline2").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline3").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline4").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline5").style.borderBottom = "2px solid transparent";
+        document.getElementById("underline6").style.borderBottom = "2px solid var(--yellow-background)";
+    }
+    // --------------------
+//  -------------------- -------------------- --------------------  
+
+
+
+
+
+// --------------------  -------------------- 
+    // 
+    
+    // -------------------- 
+
+    //  
+
+    // -------------------- 
+//  -------------------- -------------------- -------------------- 
